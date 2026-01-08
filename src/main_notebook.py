1cc70d8adacf6b90c50bef36bdfa4aef176474f0
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-01-08 15:25:29 +0100

    Added minimal interaction with OMERO

--- src/main_notebook.py
+++ src/main_notebook.py
@@ -34,7 +34,7 @@ def _(mo):
         mo.ui.text(label="OMERO username"),
         mo.ui.text(label="OMERO password", kind="password"),
         mo.ui.text(label="OMERO server URL"),
-        mo.ui.number(4064, label="OMERO port"),
+        mo.ui.number(value=4064, start=1, step=1, label="OMERO port"),
         mo.ui.text(label="Group"),
         mo.ui.checkbox(value=True, label="Secured connection")
     ])
@@ -53,6 +53,28 @@ def _(conn_params, omero_tb):
         secure=conn_params[5].value
     )
     print("Connected") if conn.connect() else print("Unable to connect")
+    return (conn,)
+
+
+@app.cell
+def _(mo):
+    data_params = mo.ui.array([
+        mo.ui.number(start=1, step=1, label="Dataset ID"),
+    ])
+    data_params
+    return (data_params,)
+
+
+@app.cell
+def _(conn, data_params, omero_tb):
+    dataset = omero_tb.get_dataset(connection=conn, dataset_id=data_params[0].value)
+    dataset_images = omero_tb.get_dataset_images(dataset=dataset)
+    print([i.getName() for i in dataset_images])
+    return
+
+
+@app.cell
+def _():
     return
 
 
