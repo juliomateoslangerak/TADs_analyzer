b7d3f8a6e22b6a4ba61306208155ce3d1e2bb37a
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-03-24 18:05:22 +0100

    Add `masks_from_labels_image_3d` and `rois_from_labels_3d` functions for 3D mask and ROI creation.

--- src/omero_toolbox.py
+++ src/omero_toolbox.py
@@ -1200,5 +1200,87 @@ def create_shape_mask(
     return mask
 
 
+def masks_from_labels_image_3d(
+    labels_3d, rgba=None, c=None, t=None, text=None, raise_on_no_mask=True
+):
+    """
+    Create a mask shape from a binary image (background=0)
+
+    :param numpy.array labels_3d: labels 3D array
+    :param rgba int-4-tuple: Optional (red, green, blue, alpha) colour
+    :param c: Optional C-index for the mask
+    :param t: Optional T-index for the mask
+    :param text: Optional text for the mask
+    :param raise_on_no_mask: If True (default) throw an exception if no mask
+           found, otherwise return an empty Mask
+    :return: An OMERO mask
+    :raises NoMaskFound: If no labels were found
+    :raises InvalidBinaryImage: If the maximum labels is greater than 1
+    """
+    rois = {}
+    for i in range(1, labels_3d.max() + 1):
+        if not np.any(labels_3d == i):
+            continue
+
+        masks = []
+        bin_img = labels_3d == i
+        # Find bounding box to minimise size of mask
+        xmask = bin_img.sum(0).sum(0).nonzero()[0]
+        ymask = bin_img.sum(0).sum(1).nonzero()[0]
+        if any(xmask) and any(ymask):
+            x0 = min(xmask)
+            w = max(xmask) - x0 + 1
+            y0 = min(ymask)
+            h = max(ymask) - y0 + 1
+            submask = bin_img[:, y0 : (y0 + h), x0 : (x0 + w)]
+        else:
+            if raise_on_no_mask:
+                raise omero_rois.NoMaskFound()
+            x0 = 0
+            w = 0
+            y0 = 0
+            h = 0
+            submask = []
+
+        for z, plane in enumerate(submask):
+            if np.any(plane):
+                mask = MaskI()
+                # BUG in older versions of Numpy:
+                # https://github.com/numpy/numpy/issues/5377
+                # Need to convert to an int array
+                # mask.setBytes(np.packbits(submask))
+                mask.setBytes(np.packbits(np.asarray(plane, dtype=int)))
+                mask.setWidth(rdouble(w))
+                mask.setHeight(rdouble(h))
+                mask.setX(rdouble(x0))
+                mask.setY(rdouble(y0))
+                mask.setTheZ(rint(z))
+
+                if rgba is not None:
+                    ch = ColorHolder.fromRGBA(*rgba)
+                    mask.setFillColor(rint(ch.getInt()))
+                if c is not None:
+                    mask.setTheC(rint(c))
+                if t is not None:
+                    mask.setTheT(rint(t))
+                if text is not None:
+                    mask.setTextValue(rstring(text))
+
+                masks.append(mask)
+
+        rois[i] = masks
+
+    return rois
+
+def rois_from_labels_3d(img, labels_3d, rgba, c=None, t=None, text=None):
+    rois = masks_from_labels_image_3d(
+        labels_3d, rgba=rgba, c=c, t=t, raise_on_no_mask=False
+    )
+
+    for label, masks in rois.items():
+        if len(masks) > 0:
+            create_roi(img=img, shapes=masks, name=f"{text}_{label}")
+
+
 def link_annotation(object_wrapper, annotation_wrapper):
     object_wrapper.linkAnnotation(annotation_wrapper)
